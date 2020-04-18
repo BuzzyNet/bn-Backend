ef8021db57d35ee080dd1222a4ebb277242e42dc
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-18 17:05:16 +0530

    setup db connection

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 var routes = require("./routes");
+
+const config = require("./config")[process.env.ENV];
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(routes);
 
-const port = process.env.PORT || 8080;
+mongoose.connect(
+  config.dbUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("connected");
+  }
+);
+
+const port = process.env.PORT || config.port;
 
 app.listen(port, () => {
   console.log("Running on port %s", port);
--- config.js
+++ config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  baseurl: "/api/v1/",
+const config = {
+  prod: {
+    baseUrl: "/api/v1/",
+    dbUrl: "mongodb://localhost:27017/Buzzy_Net",
+    host: `https://www.buzzy.io/${baseUrl}`,
+    port: 1000,
+  },
+  dev: {
+    baseUrl: "api/v1/",
+    dbUrl: "mongodb://localhost:27017/Buzzy_Net",
+    host: `http://localhost:${port}/${baseUrl}`,
+    port: 8080,
+  },
+  test: {
+    baseUrl: "/api/v1/",
+    dbUrl: "mongodb://localhost:27017/Buzzy_Net",
+    port: 2000,
+    host: `https://test.buzzy.io/${baseUrl}`,
+  },
 };
+
+module.exports = config;
