893f6145dc135b3986227d356aa9efdb7649a4ef
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-19 20:43:01 +0530

    likes API

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "UserProfile" },
   comment: { type: String },
-  likes: { type: Number },
+  likes: { type: Number, default: 0 },
   posted: { type: Date, default: new Date() },
 });
 
